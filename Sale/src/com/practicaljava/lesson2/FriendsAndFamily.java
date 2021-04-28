3da685efb741ac174212bed3d59f7e494965983e
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-04-28 12:57:36 +0200

    Lesson 2 Project Sale: FriendsAndFamily

---
+++ Sale/src/com/practicaljava/lesson2/FriendsAndFamily.java
@@ -0,0 +1,15 @@
+package com.practicaljava.lesson2;
+
+public class FriendsAndFamily {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Favorite store: Amazon");
+		System.out.println("Currently running 30% off sale!");
+		
+		System.out.println("Lava Lamps");
+		System.out.println("Running Shoes");
+		System.out.println("Thumb Drives");
+	}
+
+}
---
+++ Sale/src/module-info.java
@@ -0,0 +1,2 @@
+module sale {
+}
