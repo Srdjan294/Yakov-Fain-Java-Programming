697b7020102281d6c25c3a97ecfdc21b9ae7c884
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-04-28 14:14:14 +0200

    Basic (primitive) types of variables

---
+++ Lesson03_OOP/src/com/practicaljava/lesson3/Variables.java
@@ -0,0 +1,48 @@
+package com.practicaljava.lesson3;
+
+public class Variables {
+	
+	//BASIC TYPES OF VARIABLES
+	
+	int chairs = 12;
+	char grade = 'A';
+	boolean cancelJob = false;
+	double nationalIncome = 23863494965745.78;
+	float hourlyRate = 12.50f; //add letter f at the end, float literals
+	long totalCars = 4637283648392l; //add letter l at the end of long literals
+	
+	//8 bits
+	byte byteMin = -128;
+	byte byteMax = 127;
+	
+	//16 bits
+	short shortMin = -32768;
+	short shortMax = 32767;
+	
+	//32 bits
+	int intMin = -2147483648;
+	int intMax = 2147483647;
+	
+	//64 bits
+	long longMin = -9223372036854775808l;
+	long longMax = 9223372036854775807l;
+	
+	//32 bits
+	//float floatMin = 1.175494351 E - 38;
+	//float floatMax = 3.402823466 E + 38;
+	
+	//64 bits
+	//double doubleMin = 2.2250738585072014 E - 308;
+	//double dobuleMax = 1.7976931348623158 E + 308;
+	
+	//16 bits
+	//Unicode
+	//min = Unicode 0 
+	//max = Unicode 2 E 16
+	char sign = 'a';
+	
+	//1 bit
+	boolean one = true;
+	boolean zero = false;
+
+}
