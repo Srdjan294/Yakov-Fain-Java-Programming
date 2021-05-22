442c62cfd697f0f6903db6ff06e469428e426cf3
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-05-22 19:26:50 +0200

    Constructors

---
+++ Lesson04_ClassMethodsAndConstructors/src/com/practicaljava/lesson4/constructors/Tax.java
@@ -0,0 +1,29 @@
+package com.practicaljava.lesson4.constructors;
+
+class Tax {
+	
+	//variables
+	double grossIncome;
+	String state;
+	int dependents;
+
+	//Constructor
+	Tax(double gi, String st, int depen){
+		
+		//initialise class variables
+		grossIncome = gi;
+		state = st;
+		dependents = depen;
+	}
+	
+	public static void main(String[] args) {
+		
+		Tax t = new Tax(65000, "NJ", 3);
+		
+		t.dependents = 4;
+		
+		System.out.println(t.grossIncome + "\n" + 
+						   t.state + "\n" + 
+						   t.dependents);
+	}
+}
